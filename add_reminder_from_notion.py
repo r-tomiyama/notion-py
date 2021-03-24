57b68528dd275dcc6d487492329931d9749d8504
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-03-24 21:37:51 +0900

    change: クラスベースで作成するカレンダーを変えられるようにした

--- add_reminder_from_notion.py
+++ add_reminder_from_notion.py
@@ -1,21 +1,22 @@
 from __future__ import print_function
 import datetime
 import pickle
+import sys
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+from abc import ABCMeta, abstractmethod
 
 from notion.client import NotionClient
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+args = sys.argv
+
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-
 token_v2 = os.environ.get("TOKEN")
-url = os.environ.get("TODO_DATABASE_URL")
-calendarId = os.environ.get("CALENDAR_ID")
 
 # Notion
 client = NotionClient(token_v2)
@@ -60,49 +61,131 @@ def fetch_calendars(now):
     return event_names
 
 
-def get_target_tasks(cv, now):
-    return list(
-        filter(
-            lambda task: task.deadline and datetime.datetime.combine(
-                task.deadline.start,
-                datetime.time(0)) > now and task.name not in fetch_calendars(now),
-            fetch_new_records(
-                cv,
-                now)))
-
-
-def create_calendar(name, date):
-    # TODO eventではなくreminderにしたい
-    event = {
-        'summary': name,
-        'description': 'Notionからの自動連携',
-        'start': {
-            'date': date,
-        },
-        'end': {
-            'date': date,
-        },
-    }
-
-    print('新しいリマインダー: ' + name + ', ' + date)
+def create_calendar(event, calendarId):
+    print('新しいリマインダー: ')
+    print(event)
     event = service.events().insert(calendarId=calendarId, body=event).execute()
-    print(event['id'])
+    return event['id']
     # TODO 失敗しても例外復帰させる
 
 
-def main():
-    cv = client.get_collection_view(url)
+class TableOperation(metaclass=ABCMeta):
+    @abstractmethod
+    def filter_records(self):
+        # 関数化する必要ないかも
+        pass
+
+    def generate_event(self):
+        pass
+
+
+class ToDoOperation(TableOperation):
+    url = os.environ.get("TODO_URL")
+    calendar_id = os.environ.get("TODO_CALENDAR_ID")
+    calendar_log_file = 'logs/todo_calendar.txt'
+
+    def filter_records(self, records, now, logs):
+        return list(
+            filter(
+                lambda task: task.deadline and datetime.datetime.combine(
+                    task.deadline.start,
+                    datetime.time(0)) > now and task.name not in fetch_calendars(now),
+                records))
+
+    def generate_event(self, record):
+        # TODO eventではなくreminderにしたい
+        return {
+            'summary': record.name,
+            'description': 'Notionからの自動連携',
+            'start': {
+                'date': str(record.deadline.start),
+            },
+            'end': {
+                'date': str(record.deadline.start),
+            },
+        }
+
+
+class MenuOperation(TableOperation):
+    url = os.environ.get("MENU_URL")
+    calendar_id = os.environ.get("MENU_CALENDAR_ID")
+    calendar_log_file = 'logs/menu_calendar.txt'
+
+    def filter_records(self, records, now, logs):
+        return list(
+            filter(
+                lambda menu: menu.date and
+                datetime.datetime.combine(
+                    menu.date.start,
+                    datetime.time(0)) > now
+                    and menu.id not in logs,
+                records))
+
+    def generate_event(self, record):
+        if record.when == '朝食':
+            date = datetime.datetime.combine(
+                record.date.start, datetime.time(8))
+        elif record.when == '昼食':
+            date = datetime.datetime.combine(
+                record.date.start, datetime.time(12))
+        else:
+            date = datetime.datetime.combine(
+                record.date.start, datetime.time(18))
+
+        return {
+            'summary': '[{when}] {menu}'.format(when=record.when, menu=','.join(record.menu)),
+            'description': '食材: {ingredients}\nレシピ: {recipe}\n※Notionからの自動連携'.format(ingredients=','.join(record.ingredients), recipe=record.recipe),
+            # TODO 残しておく食材も書きたい
+            'start': {
+                'dateTime': date.isoformat(),
+                'timeZone': 'Japan',
+            },
+            'end': {
+                'dateTime': date.isoformat(),
+                'timeZone': 'Japan',
+            },
+            "reminders": {
+                "useDefault": False,
+                "overrides": [
+                    {
+                        "method": 'email',
+                        "minutes": 10
+                    }
+                ]
+            }
+        }
+
+
+def main(operation_key):
+    if operation_key == 'todo':
+        operation = ToDoOperation()
+    elif operation_key == 'menu':
+        operation = MenuOperation()
+    else:
+        raise Exception('オペレーション名の指定が間違っている')
+
+    cv = client.get_collection_view(operation.url)
     now = datetime.datetime.utcnow()
 
-    targets = get_target_tasks(cv, now)
+    f = open(operation.calendar_log_file, 'r+')
+    target_records = operation.filter_records(fetch_new_records(cv, now), now, list(map(lambda v: v[:-1], f.readlines())))
 
-    if len(targets) > 0:
-        for task in targets:
-            create_calendar(task.name, str(task.deadline.start))
-            # TODO 更新も可能にしたい
+    if len(target_records) > 0:
+        for record in target_records:
+            calendar_id = create_calendar(
+                operation.generate_event(record),
+                operation.calendar_id)
+            print(calendar_id)
+            f.write(f'{record.id}\n')
+            # calendar_idも記録する
+            # TODO 更新することも可能にしたい
     else:
-        print('リマインダー作成なし')
+        print('カレンダー作成なし')
+    f.close()
 
 
 if __name__ == '__main__':
-    main()
+    if len(args) == 2:
+        main(args[1])
+    else:
+        raise Exception('引数の数が違う')
